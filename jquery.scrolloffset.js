765e6bb39140f32c90c6ac20623dffc2068156a9
Emmanuel B <contact@emmanuelbeziat.com>
2014-05-27 04:59:23 +0200

    Propreté des variables

--- jquery.scrolloffset.js
+++ jquery.scrolloffset.js
@@ -2,6 +2,7 @@
  * Plugin d'ancres animées avec décalage vertical
  * Version : 1.2
  * Par Emmanuel "Manumanu" B. (www.emmanuelbeziat.com)
+ * https://github.com/RhooManu/jQuery-ScrollOffset
  **/
 
 (function($) {
@@ -12,13 +13,13 @@
 		}, params);
 
 		this.each(function() {
-			var lien = $(this);
-			var ancre = lien.attr("href");
-			var hauteur = $(ancre).offset().top;
-			var decalage = hauteur - params.offset;
-			var speed = params.speed;
-			var easing = params.easing;
-			var page = $('html, body');
+			var lien = $(this),
+				ancre = lien.attr("href"),
+				hauteur = $(ancre).offset().top,
+				decalage = hauteur - params.offset,
+				speed = params.speed,
+				easing = params.easing,
+				page = $('html, body');
 
 			lien.on('click', function(event) {
 				page.animate({
