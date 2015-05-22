1b6ca50e24d5a830a571e36cd899db7419c496cb
Emmanuel Béziat <emmanuel@italic.fr>
2015-05-22 17:52:59 +0200

    v2.0 jquery prototype

--- src/js/jquery.scrolloffset.js
+++ src/js/jquery.scrolloffset.js
@@ -1,42 +1,61 @@
 /*!
- * Plugin d'ancres animées avec décalage vertical
- * Version : 1.2
- * Par Emmanuel "Manumanu" B. (www.emmanuelbeziat.com)
+ * Animate anchors to make them smooth, and allows offset
+ * Version : 2.0
+ * Emmanuel "Manumanu" B. (www.emmanuelbeziat.com)
  * https://github.com/EmmanuelBeziat/jQuery-ScrollOffset
  **/
 
 ;(function($, undefined) {
 	"use strict";
 
-	$.fn.scrollOffset = function(params) {
-
-		// valeurs par défauts des options
-		params = $.extend({
+	/**
+	 * Default values
+	 */
+	var pluginName = 'scrollOffset',
+		defaults = {
 			offset: 0,
 			speed: 400
-		}, params);
+		};
 
-		this.each(function() {
+	/**
+	 * Constructor
+	 */
+	var Plugin = function(element, options) {
+		this.element = element;
 
-			// Variables
-			var lien = $(this),
-				ancre = lien.attr('href'),
-				hauteur = $(ancre).offset().top,
-				decalage = hauteur - params.offset,
-				speed = params.speed,
-				page = $('html, body');
+		this.settings = $.extend({}, defaults, options);
 
-			// Gestion de l'événement clic
-			lien.on('click', function(event) {
-				page.animate({
-					scrollTop: (decalage)
-				}, speed);
-				event.preventDefault();
+		this._defaults = defaults;
+		this._name = pluginName;
+
+		this.init();
+	};
+
+	$.extend(Plugin.prototype, {
+		init: function() {
+			var plugin = this;
+
+			$(plugin.element).on('click', function() {
+				plugin.animate();
 			});
+		},
 
+		animate: function() {
+			$('html, body').animate({
+				scrollTop: (this.settings.offset)
+			}, this.settings.speed);
+		}
+	});
+
+	/**
+	 * jQuery plugin wrapper
+	 */
+	$.fn[pluginName] = function(options) {
+
+		return this.each(function() {
+			if (!$.data(this, 'plugin_' + pluginName)) {
+				$.data(this, 'plugin_' + pluginName, new Plugin(this, options));
+			}
 		});
-
-		// Chainage jQuery
-		return this;
 	};
 })(jQuery);
